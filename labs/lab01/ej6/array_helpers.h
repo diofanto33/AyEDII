a187d58a2e4a1ced34d7f9c8c7a4539b5852f7f8
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-03-27 11:17:37 -0300

    ej6 done

--- labs/lab01/ej6/array_helpers.h
+++ labs/lab01/ej6/array_helpers.h
@@ -1,11 +1,12 @@
-#ifndef _ARRAY_HELPERS_H_
-#define _ARRAY_HELPERS_H_
+#ifndef ARRAY_HELPERS_H
+#define ARRAY_HELPERS_H
 
 #include <stdbool.h>
 
 unsigned int array_from_file(int array[],
                              unsigned int max_size,
-                             const char *filepath); 
+                             const char *filepath);
+
 
 void array_dump(int a[], unsigned int length);
 
@@ -13,5 +14,6 @@ bool array_is_sorted(int a[], unsigned int length);
 
 void array_swap(int a[], unsigned int i, unsigned int j);
 
-#endif
+void reverse_array(int a[], unsigned int length);
 
+#endif /* ARRAY_HELPERS_H */

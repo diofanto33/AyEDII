50b24d98ca74e7ccfb669a320f116c193d7f3c84
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-04-08 17:06:26 -0300

    [UPD] insertion_sort and quick_sort solution

--- labs/lab02/ej4/sort.c
+++ labs/lab02/ej4/sort.c
@@ -7,43 +7,93 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
-static unsigned int min_pos_from(int a[], unsigned int i, unsigned int length) {
+static unsigned int 
+min_pos_from(int a[], unsigned int i, unsigned int length)
+{
     unsigned int min_pos = i;
-    for (unsigned int j = i + 1; j < length; ++j) {
-        if (!goes_before(a[min_pos],a[j])) {
+    for (unsigned int j = i + 1; j < length; ++j) 
+	{
+        if (!goes_before(a[min_pos],a[j])) 
+		{
             min_pos = j;
         }
     }
     return (min_pos);
 }
 
-void selection_sort(int a[], unsigned int length) {
-    for (unsigned int i = 0u; i < length; ++i) {
+void 
+selection_sort(int a[], unsigned int length)
+{
+    for (unsigned int i = 0u; i < length; ++i)
+	{
         unsigned int min_pos = min_pos_from(a, i, length);
         swap(a, i, min_pos);
     }
 }
 
 
-static void insert(int a[], unsigned int i) {
-    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+static void 
+insert(int a[], unsigned int i)
+{
+	unsigned int j = i;
+	while(j > 0 && goes_before(a[j], a[j-1]))
+	{
+		swap(a, j-1, j);
+		j = j - 1;
+	}
 }
 
-void insertion_sort(int a[], unsigned int length) {
-    for (unsigned int i = 1u; i < length; ++i) {
+void 
+insertion_sort(int a[], unsigned int length)
+{
+    for (unsigned int i = 1u; i < length; ++i)
+	{
         insert(a, i);
-    }
+    	assert(array_is_sorted(a, i));
+	}
 }
 
-
-static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+static unsigned int 
+partition(int a[], unsigned int izq, unsigned int der)
+{
+	unsigned int ppiv = izq;
+	unsigned int i = izq + 1u;
+	unsigned int j = der;
+	while(i <= j)
+	{
+		if(goes_before(a[i], a[ppiv]))
+		{
+			i = i + 1u;
+		}
+		else if(goes_before(a[ppiv], a[j]))
+		{
+			j = j - 1u;
+		}
+		else if(goes_before(a[ppiv], a[i]) && goes_before(a[j], a[ppiv]))
+		{
+			swap(a, i, j);
+			i = i + 1u;
+			j = j - 1u;
+		}
+	}
+	swap(a, ppiv, j);
+	ppiv = j;
+	return ppiv;
 }
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 2 */
+static void 
+quick_sort_rec(int a[], unsigned int izq, unsigned int der)
+{
+	if(der > izq)
+	{
+		unsigned int ppiv = partition(a, izq, der);
+		quick_sort_rec(a, izq, ppiv == 0u ? 0u : ppiv - 1u);
+		quick_sort_rec(a, ppiv+1, der);
+	}
 }
 
-void quick_sort(int a[], unsigned int length) {
+void 
+quick_sort(int a[], unsigned int length) 
+{
     quick_sort_rec(a, 0u, (length == 0u) ? 0u : length - 1u);
 }

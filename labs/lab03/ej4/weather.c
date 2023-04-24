bf1861566e562ce0c30361427df79c1c0f594ec0
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-04-24 11:19:18 -0300

    [UPD] weather_from_file Solution

--- labs/lab03/ej4/weather.c
+++ labs/lab03/ej4/weather.c
@@ -8,9 +8,20 @@
 static const int AMOUNT_OF_WEATHER_VARS = 6 ;
 
 Weather weather_from_file(FILE* file)
-{
+{	
+	int res;
     Weather weather;
-    /* Completar aqui */
+    res = fscanf(file, EXPECTED_WEATHER_FILE_FORMAT, &weather._average_temp, 
+											  	     &weather._max_temp,
+											  		 &weather._min_temp,
+											  		 &weather._pressure,
+											  		 &weather._moisture,
+											  		 &weather._rainfall);
+	if(res != AMOUNT_OF_WEATHER_VARS)
+	{
+		fprintf(stderr, "Invalid array");
+		exit(EXIT_FAILURE);
+	} 
     return weather;
 }
 

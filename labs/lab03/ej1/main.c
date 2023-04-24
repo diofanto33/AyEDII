bb024f1efdc00ae92395b4e6313573fbd41b62b4
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-04-24 19:33:13 -0300

    [UPD] cast

--- labs/lab03/ej1/main.c
+++ labs/lab03/ej1/main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 /** @brief structure which represent a person */
+
 typedef struct _person {
     int age;
     char name_initial;
@@ -21,24 +22,19 @@ int main(void) {
     int x = 1;
     person_t m = {90, 'M'};
     int a[] = {0, 1, 2, 3};
+	
+	void *r;
+	r = &m;
+	((person_t*) r)->age = 100;
+	((person_t*) r)->name_initial = 'F';
+	r = &x;
+	*((int *) r) = 9;
+	r = a;
+	*(((int*) r)+1) = 42;
 
-    /* Completar aquí:
-       Al finalizar la ejecución las variables deben tener los siguientes valores
-       x = 9
-       m = (100, F)
-       a[1] = 42
-
-       Las restricciones son:
-       - No usar las variables 'x', 'm' y 'a' en la parte izquierda de alguna asignación.
-       - Se pueden agregar líneas de código, pero no modificar las que ya existen.
-       - Se pueden declarar hasta 2 punteros.
-       AYUDA: podes usar GDB para consultares valores de variables y la dirección de memoria donde estan alojadas.
-    */
-
-    printf("x = %d\n", x);
+	printf("x = %d\n", x);
     printf("m = (%d, %c)\n", m.age, m.name_initial);
     printf("a[1] = %d\n", a[1]);
 
     return EXIT_SUCCESS;
 }
-

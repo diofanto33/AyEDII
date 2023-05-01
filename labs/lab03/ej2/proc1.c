484dc6f70867c73c239c4f8b9f92fe4190796af1
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-01 12:46:10 -0300

    [UPD] done

--- labs/lab03/ej2/proc1.c
+++ labs/lab03/ej2/proc1.c
@@ -1,17 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void absolute(int x, int y) {
-    //
-    // Completar aquí
-    //
+/*
+ *  Este programa no sirve para nada
+ *  No va usar magia.
+ *
+ *  @topic: "Paso de parametros por valor y por referencia en C" 
+ * 	
+ * 	En C, los parametros se pasan por valor, es decir, se crea una copia del valor
+ * 	original y se trabaja con ella.
+ * 	Esto significa que si se modifica el valor de la copia, el valor original
+ * 	no se modifica.
+ *
+ * 	Por otro lado				
+ * 	Para pasar un parametro por referencia, es decir, para que se modifique el valor
+ * 	original, se debe pasar la direccion de memoria del valor original.
+ * 	Para ello, se utiliza el operador &, conocido como operador de direccion o referencia.
+ * 	El operador & devuelve la direccion de memoria de la variable.
+ * 	
+ * 	Para que la funcion reciba la direccion de memoria, se debe declarar
+ * 	el parametro como un puntero.
+ * 											
+ * 	Para que la funcion modifique el valor original, se debe utilizar
+ * 	el operador * conocido como operador de indireccion o desreferencia.
+ * 	
+ * 	El operador * devuelve el valor alojado en la direccion de memoria
+ * 
+ * 	@see: https://www.youtube.com/watch?v=4IabpaIObzM&t=32s
+ *	
+ * */
+
+void 
+absolute(int x, int y)
+{
+	if(x =< 0)
+	{
+		y = x;
+	}
+	else
+	{
+		y = -x
+	}
 }
 
-int main(void) {
+int 
+main(void)
+{
     int a=0, res=0;
-    //
-    // Completar aquí
-    //
+    a = -10;
+	absolute(a, res);
+	printf("%d", res);
     return EXIT_SUCCESS;
 }
-
--- labs/lab03/ej2/proc2.c
+++ labs/lab03/ej2/proc2.c
@@ -1,17 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void absolute(int x, int *y) {
-    //
-    // Completar aquí
-    //
+void 
+absolute(int x, int *y)
+{
+    if(x >= 0)
+	{
+		*y = x;
+	}
+	else
+	{
+		*y = - x;
+	}
 }
 
-int main(void) {
-    int a=0, res=0;  // No modificar esta declaración
-    //
-    // Completar aquí
-    //
+int 
+main(void) 
+{
+    int a=0, res=0;  
+	a=-10;
+	absolute(a, &res);
+	printf("%d", res);
     return EXIT_SUCCESS;
 }
 
+/*
+ * Al declarar absolute(int x, int *y) estamos haciendo paso por referencia
+ * a la variable (y) i.e. la funcion toma la direccion de memoria de la 
+ * variable (y) para asi poder modificarla, de esta manera decimos que el 
+ * parametro int *y es de la forma in/out.
+ *
+ * Por otro lado la variable (x) se pasa como valor, la funcion recibe una
+ * copia de (x), es capaz de usar esa copia pero no de modificar la variable
+ * (x) 
+ *
+ * */

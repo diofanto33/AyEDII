0b8a46463cc2c93c5b4d958f2978ab8f79240d8f
Diego Rodríguez <91631854+diofanto33@users.noreply.github.com>
2023-05-01 12:28:28 -0300

    Update weather.c

--- labs/lab03/ej4/weather.c
+++ labs/lab03/ej4/weather.c
@@ -7,16 +7,17 @@
 
 static const int AMOUNT_OF_WEATHER_VARS = 6 ;
 
-Weather weather_from_file(FILE* file)
+Weather 
+weather_from_file(FILE* file)
 {	
-	int res;
+    int res;
     Weather weather;
     res = fscanf(file, EXPECTED_WEATHER_FILE_FORMAT, &weather._average_temp, 
-											  	     &weather._max_temp,
-											  		 &weather._min_temp,
-											  		 &weather._pressure,
-											  		 &weather._moisture,
-											  		 &weather._rainfall);
+						     &weather._max_temp,
+				  		     &weather._min_temp,
+					  	     &weather._pressure,
+     				  		     &weather._moisture,
+				  		     &weather._rainfall);
 	if(res != AMOUNT_OF_WEATHER_VARS)
 	{
 		fprintf(stderr, "Invalid array");
@@ -25,8 +26,13 @@ Weather weather_from_file(FILE* file)
     return weather;
 }
 
-void weather_to_file(FILE* file, Weather weather)
+void 
+weather_to_file(FILE* file, Weather weather)
 {
     fprintf(file, EXPECTED_WEATHER_FILE_FORMAT, weather._average_temp, 
-            weather._max_temp, weather._min_temp, weather._pressure, weather._moisture, weather._rainfall);
+            					weather._max_temp,
+	    					weather._min_temp,
+	    					weather._pressure,
+	    					weather._moisture,
+	    					weather._rainfall);
 }

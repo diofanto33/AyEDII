a05c6326777763f1a181e778760980ea92e4eea0
Diego Rodríguez <91631854+diofanto33@users.noreply.github.com>
2023-05-04 15:15:11 -0300

    [UPD] flight

--- labs/parciales/Parcial 1 2022-04-28 kickstart (Tema A)/flight.c
+++ labs/parciales/Parcial 1 2022-04-28 kickstart (Tema A)/flight.c
@@ -12,15 +12,14 @@ flight_from_file(FILE* file, char code)
 {
     Flight flight;
     flight.code = code;
-	int res = fscanf(file, EXPECTED_FLIGHT_FILE_FORMAT, 
-											&flight.type, 
-											&flight.hour, 
-											&flight.passengers_amount);
-	if(res != AMOUNT_OF_FLIGHT_VARS)
-	{
-		perror("Error al leer el formato\n");
-		exit(EXIT_FAILURE);
-	}
-	flight.hour = flight.hour -1;
+    int res = fscanf(file, EXPECTED_FLIGHT_FILE_FORMAT, &flight.type, 
+			         		        &flight.hour, 
+							&flight.passengers_amount);
+    if(res != AMOUNT_OF_FLIGHT_VARS)
+    {
+        perror("Error al leer el formato\n");
+	exit(EXIT_FAILURE);
+    }
+    flight.hour = flight.hour -1;
     return flight;
 }

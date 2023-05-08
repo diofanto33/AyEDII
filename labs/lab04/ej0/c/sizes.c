50567b3df28a077075b93e17c92a6f22a9791a9e
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-08 10:50:31 -0300

    [UPD] print sizeof

--- labs/lab04/ej0/c/sizes.c
+++ labs/lab04/ej0/c/sizes.c
@@ -11,15 +11,29 @@ print_data(data_t d) {
            d.name, d.age, d.height);
 }
 
-int main(void) {
-
+int
+main(void)
+{
     data_t messi = {"Leo Messi", 35, 169};
     print_data(messi);
 
     printf("name-size  : %lu bytes\n"
            "age-size   : %lu bytes\n"
            "height-size: %lu bytes\n"
-           "data_t-size: %lu bytes\n", /* Completar */);
+           "data_t-size: %lu bytes\n", sizeof(messi.name),
+		   							   sizeof(messi.age),
+		  							   sizeof(messi.height),
+									   sizeof(messi));
+	
+	data_t pru;
+	printf("\ndata_t-size of pru: %lu bytes\n", sizeof(pru));
+	printf("name-size of pru  : %lu bytes\n", sizeof(pru.name));
+	
+	char c;
+	printf("\nchar-size: %lu bytes\n", sizeof(c));
+	
+	char a[10];
+	printf("\nchar[10]-size: %lu bytes\n", sizeof(a));
 
     return EXIT_SUCCESS;
 }

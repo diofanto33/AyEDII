e2bb57fc42922eb0559b7e578a6938007ea13107
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-20 20:18:14 -0300

    [ADD] Makefile

---
+++ labs/lab05/acid_ej1b/stack.h
@@ -0,0 +1,81 @@
+/**
+*  @file stack.h
+*  @brief TAD Stack definition
+*/
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#include <stdbool.h>
+
+/**
+* @brief Stack type definition
+*/
+typedef struct _s_stack *stack;
+
+/**
+* @brief Stack elements type definition
+*/
+typedef int stack_elem;
+
+/**
+* @brief Creates an empty stack
+* @return An empty stack
+*/
+stack stack_empty();
+
+/**
+* @brief Inserts an element at the top of the stack
+* @param s A stack
+* @param e An element to push into the stack
+* @return The new stack with 'e' at the top
+*/
+stack stack_push(stack s, stack_elem e);
+
+/**
+* @brief Removes the element at the top of the stack
+* @param s A stack
+* @return The new stack with the top element removed
+* @note Only applies to non-empty stacks
+*/
+stack stack_pop(stack s);
+
+/**
+* @brief Returns the size of the stack
+* @param s A stack
+* @return The size of the stack
+*/
+unsigned int stack_size(stack s);
+
+/**
+* @brief Returns the element at the top of the stacks
+* @param s A stacks
+* @return The element at the top of the stack
+* @note Only applies to non-empty stacks
+*/
+stack_elem stack_top(stack s);
+
+/**
+* @brief Check if the given stack is empty
+* @param s A stack
+* @return true if the stack is empty, false otherwise
+*/
+bool stack_is_empty(stack s);
+
+/**
+* @brief Creates an array with all the elements of the stack
+* @param s A stack
+* @return An array containing all the elements of the stack. The stack top element
+* becomes the rightmost element of the array. The size of the resulting
+* array is determined by 'stack_size(s)'
+*/
+stack_elem *stack_to_array(stack s);
+
+/**
+* @brief Destroys the stack
+* @param s A stack
+* @note All memory resources are freed
+*/
+stack stack_destroy(stack s);
+
+
+#endif

dfa97d1815b65d375ee4ca96f2e22dfe75f19075
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-22 18:45:59 -0300

    [UPD] Removed line break

--- labs/lab05/ej4/b/readstr.c
+++ labs/lab05/ej4/b/readstr.c
@@ -4,13 +4,21 @@
 
 #define MAX_LENGTH 20
 
-int main(void) {
-    char user_input[MAX_LENGTH];
+int
+main(void) 
+{
+	char user_input[MAX_LENGTH];
 
+	// Pedimos el nombre y apellido
     printf("Ingrese su nombre y apellido: ");
-    scanf("%s", user_input);
+	// Leemos el nombre y apellido
+    fgets(user_input, MAX_LENGTH, stdin);
 
-    printf("Te damos la bienvenida %s a este maravilloso programa!\n", user_input);
+	// Eliminamos el salto de linea
+	user_input[strlen(user_input) - 1] = '\0';
+    
+	// Imprimimos el mensaje de bienvenida
+	printf("Te damos la bienvenida %s a este maravilloso programa!\n", user_input);
 
     return EXIT_SUCCESS;
 }

d7cfebc9518e8ca0ba8f0f660822c31b551e4b48
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-25 21:29:46 -0300

    [ADD] implementation using arrays

--- labs/lab05/ej1b_array/stack.c
+++ labs/lab05/ej1b_array/stack.c
@@ -4,64 +4,36 @@
 
 #include "stack.h"
 
-struct _s_stack_node {
-    stack_elem elem;
-    struct _s_stack_node* next;
-};
+/*
+ *  @invrep():
+ * 	
+ * 	La pila no puede tener mas de MAX_STACK_SIZE elementos
+ *  
+ *  0 <= s->size < MAX_STACK_SIZE
+ *
+ * */
+
+#define MAX_STACK_SIZE 100
 
 struct _s_stack {
-    unsigned int size;
- 	struct _s_stack_node* first_node;
+	stack_elem array[MAX_STACK_SIZE];
+	unsigned int size;
 };
-
-/* @brief 
- * Private function to check the representation invariant of the stack ADT
- * @param s A stack 
- * @return true if the representation invariant holds
- * @return false otherwise
- */
-
-static bool 
-invrep(stack s)
-{	
-	bool b = (s != NULL);
-	if(b)
-	{	
-		struct _s_stack_node *p = s->first_node;
-		unsigned int length = 0u;
-		while(p != NULL)
-		{
-			length = length + 1u;
-			p = p->next;
-		}
-		b = (s->size == length);
-	}
-	return(b);
-}
 
 stack
 stack_empty()
-{	
-	stack s = calloc(1, sizeof(stack));
-	s->first_node = NULL;
-
-	assert(invrep(s));
-
+{
+	stack s = calloc(1, sizeof(struct _s_stack));
+	s->size = 0;
 	return(s);
 }
 
 stack
 stack_push(stack s, stack_elem e)
 {
-	assert(invrep(s));
-
-	struct _s_stack_node *sp = malloc(sizeof(struct _s_stack_node));
-	sp->elem = e;
-	sp->next = s->first_node;
-	s->first_node = sp;
-	s->size = s->size + 1u;	
-
-	assert(invrep);
+	assert(s->size < MAX_STACK_SIZE);
+	s->array[s->size] = e;
+	s->size = s->size + 1;
 
 	return(s);
 }
@@ -69,30 +41,22 @@ stack_push(stack s, stack_elem e)
 stack
 stack_pop(stack s)
 {
-	assert(invrep(s));	
-
-	struct _s_stack_node *sp = NULL;
-	sp = s->first_node;
-	s->first_node = s->first_node->next;
-	s->size = s->size -1u;
-	free(sp);
-	sp = NULL;
-
+	assert(s->size != 0);	
+	s->size = s->size - 1;
 	return(s);
 }
 
 unsigned int 
 stack_size(stack s)
 {
-	assert(invrep(s));
 	return(s->size);
 }
 
 stack_elem
 stack_top(stack s)
 {
-	assert(invrep(s));
-	return(s->first_node->elem);
+	assert(s->size != 0);
+	return(s->array[s->size - 1]);
 }
 
 bool stack_is_empty(stack s)
@@ -104,17 +68,14 @@ stack_elem
 *stack_to_array(stack s)
 {
 	stack_elem *a = NULL;
-	if(invrep(s) && !stack_is_empty(s))
+	if(s->size)
 	{
-		a = calloc(s->size, sizeof(stack_elem));
-		unsigned int n = s->size;
-		struct _s_stack_node *sp = NULL;
-		sp = s->first_node;
-		while(n != 0)
+		unsigned int n = stack_size(s);	
+		a = calloc(n, sizeof(stack_elem));
+		while( n != 0)
 		{
-			a[n-1u] = sp->elem;
-			sp = sp->next;
-			n = n -1u;
+			a[n-1] = s->array[n-1];
+			n = n - 1u;
 		}
 	}
 	return(a);
@@ -122,18 +83,7 @@ stack_elem
 
 stack 
 stack_destroy(stack s)
-{	
-	assert(invrep(s));
-	struct _s_stack_node *sp = NULL;
-	sp = s->first_node;
-	while(sp != NULL)
-	{	
-		s->first_node = s->first_node->next;
-		free(sp);
-		sp = s->first_node;
-	}
-	sp = NULL;
-	free(s);
-	s = NULL;
+{
+	s->size = 0;	
 	return(s);
 }

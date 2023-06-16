d5be5025c1aeeeba815fc003109c35aa0a4dbba0
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-06-16 11:19:22 -0300

    [ADD] string_less done

--- labs/lab06/ej2/string.c
+++ labs/lab06/ej2/string.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,44 +10,71 @@ struct _s_string {
     unsigned int length;
 };
 
-string string_create(const char *word) {
+string 
+string_create(const char *word)
+{
     string str = NULL;
     str = calloc(1, sizeof(struct _s_string));
-    str->length = /* needs implementation */;
-    str->content = calloc(/* needs implementation */, sizeof(char));
+    assert(str != NULL);
+    str->length = strlen(word);
+    str->content = calloc(str->length + 1, sizeof(char));
+    assert(str->content != NULL);
     str->content = strncpy(str->content, word, str->length + 1);
+        
     return (str);
 }
-
-unsigned int string_length(string str) {
+ 
+unsigned int 
+string_length(string str)
+{
     return (str->length);
 }
 
-bool string_less(const string str1, const string str2) {
-    /* needs implementation */
-    return false;
-}
+bool
+string_less(const string str1, const string str2)
+{   
+    unsigned int min_length = (str1->length < str2->length) ? str1->length : str2->length;
+    unsigned int i = 0u;
+    while(i < min_length && str1->content[i] == str2->content[i])
+    {
+        i = i + 1u;
+    }
+    bool res = (i == min_length) ? (str1->length <= str2->length) : (str1->content[i] <= str2->content[i]);
 
-bool string_eq(const string str1, const string str2) {
+    return(res);
+}   
+
+bool 
+string_eq(const string str1, const string str2)
+{
     int cmp = strcmp(str1->content, str2->content);
+   
     return (cmp == 0);
 }
 
-string string_clone(const string str) {
+string 
+string_clone(const string str)
+{
     return (string_create(str->content));
 }
 
-string string_destroy(string str) {
+string 
+string_destroy(string str)
+{
     free(str->content);
     free(str);
     str = NULL;
     return (str);
 }
 
-void string_dump(string str, FILE *file) {
+void
+string_dump(string str, FILE *file)
+{
     fprintf(file, "%s", str->content);
 }
 
-const char *string_ref(string str) {
+const char 
+*string_ref(string str)
+{
     return (str->content);
 }

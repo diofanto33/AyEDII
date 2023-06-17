577cad4e3d1c30c9ff9630dfbcfe93a523aaa809
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-06-17 16:57:27 -0300

    [UPD] string_less

--- labs/lab06/ej3/string.c
+++ labs/lab06/ej3/string.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,44 +10,65 @@ struct _s_string {
     unsigned int length;
 };
 
-string string_create(const char *word) {
+string 
+string_create(const char *word)
+{
     string str = NULL;
     str = calloc(1, sizeof(struct _s_string));
-    str->length = /* needs implementation */;
-    str->content = calloc(/* needs implementation */, sizeof(char));
+    assert(str != NULL);
+    str->length = strlen(word);
+    str->content = calloc(str->length + 1, sizeof(char));
+    assert(str->content != NULL);
     str->content = strncpy(str->content, word, str->length + 1);
+        
     return (str);
 }
-
-unsigned int string_length(string str) {
+ 
+unsigned int 
+string_length(string str)
+{
     return (str->length);
 }
 
-bool string_less(const string str1, const string str2) {
-    /* needs implementation */
-    return false;
-}
+bool
+string_less(const string str1, const string str2)
+{   
+    bool res = (strcmp(str1->content, str2->content) < 0; 
+    
+    return(res);
+}   
 
-bool string_eq(const string str1, const string str2) {
+bool 
+string_eq(const string str1, const string str2)
+{
     int cmp = strcmp(str1->content, str2->content);
+   
     return (cmp == 0);
 }
 
-string string_clone(const string str) {
+string 
+string_clone(const string str)
+{
     return (string_create(str->content));
 }
 
-string string_destroy(string str) {
+string 
+string_destroy(string str)
+{
     free(str->content);
     free(str);
     str = NULL;
     return (str);
 }
 
-void string_dump(string str, FILE *file) {
-    fprintf(file, "%s", str->content);
+void
+string_dump(string str, FILE *file)
+{
+    fprintf(file, "%s ", str->content);
 }
 
-const char *string_ref(string str) {
+const char 
+*string_ref(string str)
+{
     return (str->content);
 }

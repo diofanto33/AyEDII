5b20a73a7896e256c83b86e11dbd7043dd64eb97
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-06-17 16:56:42 -0300

    [ADD] user interface

--- labs/lab06/ej3/main.c
+++ labs/lab06/ej3/main.c
@@ -23,12 +23,16 @@
 
 #define RESULT_PREFIX "\t-> "
 
-void print_help(char *program_name) {
+void 
+print_help(char *program_name) 
+{
     /* Print the usage help of this program. */
     printf("Usage: %s\n\n", program_name);
 }
 
-char print_menu(void) {
+char
+print_menu(void) 
+{
     char result = '\0';
     char *line = NULL;
 
@@ -49,7 +53,8 @@ char print_menu(void) {
            "\nPlease enter your choice: ");
 
     line = readline_from_stdin();
-    if (line != NULL) {
+    if (line != NULL)
+    {
         result = line[0];
     }
 
@@ -57,7 +62,9 @@ char print_menu(void) {
     return (result);
 }
 
-bool is_valid_option(char option) {
+bool
+is_valid_option(char option)
+{
     bool result = false;
 
     result = (option == ADD || option == REMOVE ||
@@ -68,7 +75,9 @@ bool is_valid_option(char option) {
     return (result);
 }
 
-string get_input(const char *message) {
+string
+get_input(const char *message)
+{
     string result = NULL;
     printf("\t%s: ", message);
     result = readstring_from_stdin();
@@ -76,14 +85,19 @@ string get_input(const char *message) {
     return (result);
 }
 
-dict_t on_add(dict_t current) {
+dict_t 
+on_add(dict_t current)
+{
     string definition = NULL;
     string word = NULL;
     word = get_input("Please enter the word to add into the dict");
-    if (dict_exists(current, word)) {
+    if (dict_exists(current, word))
+    {
         printf(RESULT_PREFIX "The word is already in the dict.\n");
         word = string_destroy(word);
-    } else {
+    }
+    else
+    {
         definition = get_input("Please enter the definition");
         current = dict_add(current, word, definition);
         printf(RESULT_PREFIX "The word and definition were added.\n");
@@ -91,14 +105,19 @@ dict_t on_add(dict_t current) {
     return (current);
 }
 
-dict_t on_replace(dict_t current) {
+dict_t
+on_replace(dict_t current)
+{
     string definition = NULL;
     string word = NULL;
     word = get_input("Please enter the word to replace in the dict");
-    if (!dict_exists(current, word)) {
+    if (!dict_exists(current, word))
+    {
         printf(RESULT_PREFIX "The word does not exist in the dict.\n");
         word = string_destroy(word);
-    } else {
+    } 
+    else
+    {
         definition = get_input("Please enter the new definition");
         current = dict_add(current, word, definition);
         printf(RESULT_PREFIX "The definition was replaced.\n");
@@ -106,12 +125,17 @@ dict_t on_replace(dict_t current) {
     return (current);
 }
 
-dict_t on_remove(dict_t current) {
+dict_t 
+on_remove(dict_t current)
+{
     string word =
             get_input("Please enter the word to delete from the dict");
-    if (!dict_exists(current, word)) {
+    if (!dict_exists(current, word))
+    {
         printf(RESULT_PREFIX "The word does not exist in the dict.\n");
-    } else {
+    } 
+    else
+    {
         current = dict_remove(current, word);
         printf(RESULT_PREFIX "The word was removed.\n");
     }
@@ -119,13 +143,18 @@ dict_t on_remove(dict_t current) {
     return (current);
 }
 
-dict_t on_load(dict_t current) {
+dict_t
+on_load(dict_t current)
+{
     string filename
             = get_input("Please enter the filename to load the dict from");
     dict_t other = dict_from_file(filename);
-    if (other == NULL) {
+    if (other == NULL) 
+    {
         printf("Can not load dict from filename %s\n", string_ref(filename));
-    } else {
+    } 
+    else
+    {
         printf(RESULT_PREFIX "The dictionary was successfully loaded.\n");
         current = dict_destroy(current);    /* destroy current */
         current = other;
@@ -134,70 +163,85 @@ dict_t on_load(dict_t current) {
     return (current);
 }
 
-void on_dump(dict_t current) {
+void 
+on_dump(dict_t current) 
+{
     string filename = get_input("Please enter the filename to dump the file");
     dict_to_file(current, filename);
     filename = string_destroy(filename);
     printf(RESULT_PREFIX "The dictionary was successfully dumped.\n");
 }
 
-dict_t on_empty(dict_t current) {
+dict_t 
+on_empty(dict_t current)
+{
     current = dict_remove_all(current);
     printf(RESULT_PREFIX "All words were removed\n");
     return (current);
 }
 
-void on_search(dict_t current) {
+void 
+on_search(dict_t current)
+{
     string definition = NULL;
     string word = NULL;
     word = get_input("Please enter the word to search in the dict");
     definition = dict_search(current, word);
-    if (definition == NULL) {
+    if (definition == NULL) 
+    {
         printf(RESULT_PREFIX "The word does not exist in the dict\n");
-    } else {
+    }
+    else
+    {
         printf(RESULT_PREFIX "The definition of \"%s\" is : \"%s\"\n",
                string_ref(word), string_ref(definition));
     }
     word = string_destroy(word);
 }
 
-void on_size(dict_t current) {
+void 
+on_size(dict_t current) 
+{
     printf(RESULT_PREFIX "The size of the dict is %u\n", dict_length(current));
 }
 
-int main(void) {
+int
+main(void) 
+{
     char option = '\0';
     dict_t current = dict_empty();
     /* print a simple menu and do the requested operations */
-    do {
+    do 
+    {
         option = print_menu();
-        switch (option) {
+        switch (option) 
+        {
             case ADD:
-
+                current = on_add(current);
                 break;
             case REMOVE:
-
+                current = on_remove(current);
                 break;
             case REPLACE:
-
+                current = on_replace(current);
                 break;
             case DUMP:
-
+                on_dump(current);
                 break;
             case EMPTY:
-
+                current = on_empty(current);
                 break;
             case LOAD:
-
+                current = on_load(current);
                 break;
             case SEARCH:
-
+                on_search(current);
                 break;
             case SHOW:
-
+                on_dump(current);
                 break;
             case SIZE:
-
+                on_size(current);
                 break;
             case QUIT:
                 current = dict_destroy(current);

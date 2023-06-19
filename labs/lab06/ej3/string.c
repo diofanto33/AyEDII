be5c858c6f78ec0e8f234961cb29b01173356c0e
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-06-19 17:13:39 -0300

    [UPD] string_less

--- labs/lab06/ej3/string.c
+++ labs/lab06/ej3/string.c
@@ -10,6 +10,15 @@ struct _s_string {
     unsigned int length;
 };
 
+static bool 
+invrep(string str) 
+{   
+    bool res = str != NULL;
+
+    return(res);
+}
+
+
 string 
 string_create(const char *word)
 {
@@ -19,28 +28,44 @@ string_create(const char *word)
     str->length = strlen(word);
     str->content = calloc(str->length + 1, sizeof(char));
     assert(str->content != NULL);
-    str->content = strncpy(str->content, word, str->length + 1);
-        
+    str->content = strncpy(str->content, word, str->length + 1u);
+    
+    assert(invrep(str));
+
     return (str);
 }
  
 unsigned int 
 string_length(string str)
-{
+{   
+    assert(invrep(str));
+
     return (str->length);
 }
 
 bool
 string_less(const string str1, const string str2)
 {   
-    bool res = strcmp(str1->content, str2->content) < 0; 
+    assert(invrep(str1) && invrep(str2));
     
-    return(res);
+    bool result;
+	unsigned int len_s1 = str1->length;
+	unsigned int len_s2 = str2->length;
+	unsigned int min_length = len_s1 < len_s2 ? len_s1 : len_s2;
+	unsigned int i = 0;
+	while(i < min_length && str1->content[i] == str2->content[i])
+	{
+		i = i + 1;
+	}
+	result = i == min_length ? len_s1 <= len_s2 : str1->content[i] <= str2->content[i];
+
+    return(result);
 }   
 
 bool 
 string_eq(const string str1, const string str2)
 {
+    assert(invrep(str1) && invrep(str2));
     int cmp = strcmp(str1->content, str2->content);
    
     return (cmp == 0);
@@ -49,12 +74,14 @@ string_eq(const string str1, const string str2)
 string 
 string_clone(const string str)
 {
+    assert(invrep(str));
     return (string_create(str->content));
 }
 
 string 
 string_destroy(string str)
 {
+    assert(invrep(str));
     free(str->content);
     free(str);
     str = NULL;
@@ -63,12 +90,14 @@ string_destroy(string str)
 
 void
 string_dump(string str, FILE *file)
-{
+{   
+    assert(invrep(str));
     fprintf(file, "%s ", str->content);
 }
 
 const char 
 *string_ref(string str)
 {
+    assert(invrep(str));
     return (str->content);
 }
